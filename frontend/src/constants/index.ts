869e79f20957ee3a2eec4adba12adc2cd6a1c136
elias_sisay <87943132+eliassisay@users.noreply.github.com>
2025-10-07 16:04:46 +0300

    update

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
-  import.meta.env.API_BASE_URL || "https://pharma-inventory-3.onrender.com/";
+  import.meta.env.API_BASE_URL || "https://pharma-inventory-3.onrender.com/api";
 export const APP_NAME = import.meta.env.APP_NAME || "Pharma Inventory";
